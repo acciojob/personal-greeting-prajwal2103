d83a452ecca3cdc3ab38f287518d5ae6f397f1ac
prajwal2103 <prajwaljagtap5@gmail.com>
2024-09-03 11:54:31 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+ const [name, setName] = useState('');
   return (
     <div>
-        {/* Do not remove the main div */}
+       <label htmlFor="ip">Enter your name:</label>
+       <input id='ip' type='text' onChange={(e)=>{
+        setName(e.target.value)
+       }}  />
+       <p>{name?`Hello ${name}!`:''}</p>
     </div>
   )
 }
